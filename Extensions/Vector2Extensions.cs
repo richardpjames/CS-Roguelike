06f79bd4c4dbea02511c8e93b9985ce6ff63df14
Richard James <richard@richardpjames.com>
2024-10-24 19:34:55 +0100

    Tweaks to pixel space conversions

---
+++ Extensions/Vector2Extensions.cs
@@ -0,0 +1,22 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+public static class Vector2Extensions
+{
+    // Adds a method to Vector2 objects to convert from game space to pixel space (multiply by pixels per unit)
+    public static Vector2 WorldToPixel(this Vector2 vector)
+    {
+        return vector * Game.PIXELS_PER_UNIT;
+    }
+
+    // Turns screen coordinates to world coordinates accounting for intermediate translation of pixels per inch
+    public static Vector2 ScreenToWorld(this Vector2 vector, Camera2D camera)
+    {
+        Vector2 newVector = Raylib.GetScreenToWorld2D(vector, camera) / Game.PIXELS_PER_UNIT;
+        newVector.X = MathF.Floor(newVector.X);
+        newVector.Y = MathF.Floor(newVector.Y);
+        return newVector;
+    }
+
+}
+
